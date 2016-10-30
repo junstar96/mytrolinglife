e378b4846a713d628eecf6ec8226ece9e6e958e3
junstar96 <junstar96@naver.com>
2016-10-31 00:46:36 +0900

    image_+

--- maps.py
+++ maps.py
@@ -10,12 +10,14 @@ class Map:
     def __init__(self):
         self.x, self.y = 400,340
         if self.image1 == None:
-            self.image1 = load_image("map.png")
+            self.image1 = load_image("tileboard.png")
         if self.image2 == None:
             self.image2 = load_image("map.png")
 
     def draw(self):
-        self.image1.draw(self.x, self.y, 800, 540)
+        for i in range(0,21):
+            for j in range(0, 16):
+                self.image1.draw(i*40, j*40, 40,40)
 
 
 def mapwall(a):
---
+++ police.py
@@ -0,0 +1,12 @@
+import sys
+import os
+import random
+from pico2d import *
+
+class nonplayerable:
+    protectableimage = None
+    atteckableimage = None
+
+    def __init__(self):
+        self.x, self.y = 0,0
+    pass
